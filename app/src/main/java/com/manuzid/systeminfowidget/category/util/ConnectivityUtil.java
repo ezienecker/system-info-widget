401be076ab4fbfc9c16f2b1d65be031c153af037
ezienecker <manuzidtv@gmail.com>
2017-10-15 19:01:41 +0200

    Aktualisierung der Einstellungen
    - Aktualisierung der Einstellungen implementiert, nun werden beim verlassen der ConfigActivity diese auch korrekt übernommen.
    - Dokumentation der Klassen und Methoden.

--- app/src/main/java/com/manuzid/systeminfowidget/category/util/ConnectivityUtil.java
+++ app/src/main/java/com/manuzid/systeminfowidget/category/util/ConnectivityUtil.java
@@ -23,7 +23,7 @@ public class ConnectivityUtil {
      * Get the network info
      *
      * @param context {@link Context}
-     * @return
+     * @return {@link NetworkInfo}
      */
     private static NetworkInfo getNetworkInfo(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -34,7 +34,7 @@ public class ConnectivityUtil {
      * Check if there is any connectivity to a Wifi network
      *
      * @param context {@link Context}
-     * @return
+     * @return is connected with wifi true otherwise false
      */
     public static boolean isConnectedWifi(Context context) {
         NetworkInfo info = ConnectivityUtil.getNetworkInfo(context);
@@ -45,7 +45,7 @@ public class ConnectivityUtil {
      * Check if there is any connectivity to a mobile network
      *
      * @param context {@link Context}
-     * @return
+     * @return is connected with mobile true otherwise false
      */
     public static boolean isConnectedMobile(Context context) {
         NetworkInfo info = ConnectivityUtil.getNetworkInfo(context);
@@ -53,68 +53,12 @@ public class ConnectivityUtil {
     }
 
     /**
-     * Check if there is fast connectivity
+     * Ermittelt den Netzwerk-Type
      *
-     * @param context
-     * @return
+     * @param context {@link Context}
+     * @return den Netzwerk-Typ als {@link String}
      */
-    public static boolean isConnectedFast(Context context) {
-        NetworkInfo info = ConnectivityUtil.getNetworkInfo(context);
-        return (info != null && info.isConnected() && ConnectivityUtil.isConnectionFast(info.getType(), info.getSubtype()));
-    }
-
-    /**
-     * Check if the connection is fast
-     *
-     * @param type
-     * @param subType
-     * @return
-     */
-    public static boolean isConnectionFast(int type, int subType) {
-        if (type == ConnectivityManager.TYPE_WIFI) {
-            return true;
-        } else if (type == ConnectivityManager.TYPE_MOBILE) {
-            switch (subType) {
-                case TelephonyManager.NETWORK_TYPE_1xRTT:
-                    return false; // ~ 50-100 kbps
-                case TelephonyManager.NETWORK_TYPE_CDMA:
-                    return false; // ~ 14-64 kbps
-                case TelephonyManager.NETWORK_TYPE_EDGE:
-                    return false; // ~ 50-100 kbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_0:
-                    return true; // ~ 400-1000 kbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_A:
-                    return true; // ~ 600-1400 kbps
-                case TelephonyManager.NETWORK_TYPE_GPRS:
-                    return false; // ~ 100 kbps
-                case TelephonyManager.NETWORK_TYPE_HSDPA:
-                    return true; // ~ 2-14 Mbps
-                case TelephonyManager.NETWORK_TYPE_HSPA:
-                    return true; // ~ 700-1700 kbps
-                case TelephonyManager.NETWORK_TYPE_HSUPA:
-                    return true; // ~ 1-23 Mbps
-                case TelephonyManager.NETWORK_TYPE_UMTS:
-                    return true; // ~ 400-7000 kbps
-                case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
-                    return true; // ~ 1-2 Mbps
-                case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
-                    return true; // ~ 5 Mbps
-                case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
-                    return true; // ~ 10-20 Mbps
-                case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
-                    return false; // ~25 kbps
-                case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
-                    return true; // ~ 10+ Mbps
-                // Unknown
-                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
-                default:
-                    return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
+    @NonNull
     public static String getNetworkClass(Context context) {
         TelephonyManager mTelephonyManager = (TelephonyManager)
                 context.getSystemService(Context.TELEPHONY_SERVICE);
@@ -166,6 +110,12 @@ public class ConnectivityUtil {
         return null;
     }
 
+    /**
+     * Ermittelt die Signalstärke des W-Lans mit dem der Benutzer verbunden ist.
+     *
+     * @param context {@link Context}
+     * @return Stärke des W-Lans in einer Staffelung von 5
+     */
     @NonNull
     public static String getWifiSignalStrength(Context context) {
         WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
