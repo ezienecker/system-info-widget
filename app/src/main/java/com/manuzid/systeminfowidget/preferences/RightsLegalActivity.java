110312a937c4a27e21444c5f081ef09e9b7489bf
ezienecker <manuzidtv@gmail.com>
2017-11-07 22:41:43 +0100

    Optimierung Rechtliches
    Zuwendungstext angepasst, Privacy Policy mit aufgenommen,

--- app/src/main/java/com/manuzid/systeminfowidget/preferences/RightsLegalActivity.java
+++ app/src/main/java/com/manuzid/systeminfowidget/preferences/RightsLegalActivity.java
@@ -2,8 +2,11 @@ package com.manuzid.systeminfowidget.preferences;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.manuzid.systeminfowidget.R;
+
+import java.util.Calendar;
 
 /**
  * Created by Emanuel Zienecker on 02.05.13. Copyright (c) 2013 Emanuel
@@ -15,5 +18,10 @@ public class RightsLegalActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sysinfo_config_legal);
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        TextView legalNotice = findViewById(R.id.config_legal_privat);
+        legalNotice.setText(String.format(getString(R.string.config_entries_rights_legal_privat), year));
     }
 }
